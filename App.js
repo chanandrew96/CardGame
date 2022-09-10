966b2baa4d6346a00cc56ddefcd4499afbfbf38b
chanandrew96 <chanandrew96@yahoo.com.hk>
2022-09-10 18:26:34 +0800

    Add navigation

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import 'react-native-gesture-handler';
+// react-native-gesture-handler must be on the top line
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem, } from '@react-navigation/drawer';
+
+import Home from './views/Home.js';
+import GameView from './views/Games/GameView.js';
+import GameList from './views/Games/GameList.js';
+import Setting from './views/Setting.js';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Drawer.Navigator useLegacyImplementation={true} initialRouteName="Home">
+          <Drawer.Screen name="Home" component={Home} />
+          <Drawer.Screen name="Setting" component={Setting} />
+        </Drawer.Navigator>
+        {/* <Tab.Navigator>
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="Setting" component={Setting} />
+        </Tab.Navigator> */}
+        {/* <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Game List" component={GameList} />
+        </Stack.Navigator> */}
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
